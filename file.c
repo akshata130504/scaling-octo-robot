99990cbfc36a5a66d91f1c02c406aca297527b9b
akshata130504 <152254752+akshata130504@users.noreply.github.com>
2023-12-04 22:37:59 -0800

    Create file.c

---
+++ file.c
@@ -0,0 +1,7 @@
+Dual mode operation allows os to protect itself and other system components.
+User mode and kernel mode.
+Kernel mode is also called as supervisory mode,system mode or privileged mode.
+Operating system-system-dual mode.
+Mode bit is provided by hardware.
+There is a bit called modebit which is added to hardware of the computer.
+That executes on behalf of operating system.
